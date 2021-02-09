e64979c35e7168ba2cbf723ae861f54a6113b10e
tsaristbomba <andrexerdna@gmail.com>
2021-02-09 11:06:04 -0300

    about image optimization

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -36,7 +36,7 @@ const Profile = ({ alt, skillX, skill, socials, contact }) => {
           node {
             childImageSharp {
               fluid {
-                ...GatsbyImageSharpFluid
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
               }
             }
           }
